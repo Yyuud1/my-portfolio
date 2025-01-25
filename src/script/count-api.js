6e124c6b1e6da219f6103364d55669d68dd6d4c5
Yyuud1 <yudaaanugrah1@gmail.com>
2025-01-25 11:41:33 +0700

    my web portfolio

--- src/script/count-api.js
+++ src/script/count-api.js
@@ -1,11 +1,23 @@
-const countVisit = document.querySelector("#count");
+// const countVisit = document.querySelector("#count");
 
-const timesVisit = () => {
-  fetch("https://api.countapi.xyz/update/yudagrh/abcd1234/?amount=1")
-    .then((response) => response.json())
-    .then((response) => {
-      countVisit.innerHTML = response.value;
+// const timesVisit = () => {
+//   fetch("https://api.countapi.xyz/update/yudagrh/abcd1234/?amount=1")
+//     .then((response) => response.json())
+//     .then((response) => {
+//       countVisit.innerHTML = response.value;
+//     });
+// };
+
+// timesVisit();
+
+const visit = document.getElementById("count");
+
+updateVisit();
+
+function updateVisit() {
+  fetch("https://api.countapi.xyz/update/yyuud/mouse/?amount=1")
+    .then((res) => res.json())
+    .then((res) => {
+      visit.innerHTML = res.value;
     });
-};
-
-timesVisit();
+}
